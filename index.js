91110acb5a9eedcaf0696153baf1ba8bec607492
Tate Maguire <tatemaguir@gmail.com>
2025-12-15 22:49:13 -0800

    initial commit based on my old-portfolio repository

---
+++ index.js
@@ -0,0 +1,5 @@
+const background = document.getElementById("background");
+const ctx = background.getContext("2d");
+
+ctx.fillStyle = "#9d8ea9ff";
+ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
