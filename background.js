9a7016caa9c8c50710e261a8e0505eb93a31261b
Tate Maguire <tatemaguir@gmail.com>
2025-12-22 15:45:18 -0800

    background and menu changes

--- background.js
+++ background.js
@@ -5,12 +5,21 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "rgb(79, 67, 89)";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+drawBackground
+function drawBackground() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
 
-document.addEventListener("resize", () => {
-    console.log("RESIZE");
+globalThis.addEventListener("resize", () => {
     canvas.width = globalThis.innerWidth;
     canvas.height = globalThis.innerHeight;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    drawBackground();
 });
+
+function draw(timestamp) {
+    drawBackground();
+    requestAnimationFrame(draw);
+}
+
+requestAnimationFrame(draw);
